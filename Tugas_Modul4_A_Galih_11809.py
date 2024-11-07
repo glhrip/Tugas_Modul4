d50c9abeab4de324cb1e0b55c9bd6592efffdeee
glhrip <220711809@students.uajy.ac.id>
2024-11-07 17:48:19 +0700

    Update Tugas_Modul4_A_Galih_11809.py

--- Tugas_Modul4_A_Galih_11809.py
+++ Tugas_Modul4_A_Galih_11809.py
@@ -46,7 +46,6 @@ if uploaded_file is not None:
     st.markdown("<h1 style='text-align: center;'>Unsupervised Learning - Galih</h1>", unsafe_allow_html=True)
     st.dataframe(input_data)
 
-    model_directory = r'D:\PMDPM\Unsupervised Learning (Praktek)\Unsupervised Learning (Praktek)\Tugas4_A_11809'
     model_path = {
         "AGG_model": os.path.join(model_directory, r'AGG_model.pkl'),
         "KMeans_model": os.path.join(model_directory, r'KMeans_model.pkl'),
